e67a4837796c6fa742a34e344bcb545fd9bf35ac
Austin Tao <taoaustin@yahoo.ca>
2020-07-25 16:08:41 -0400

    Implemented all face turns, TODO: timer, reset, scrambler

--- app/src/main/java/com/example/magic_cube_app/MainActivity.java
+++ app/src/main/java/com/example/magic_cube_app/MainActivity.java
@@ -1,3 +1,4 @@
+//Author: Austin Tao 2020
 package com.example.magic_cube_app;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -31,8 +32,8 @@ public class MainActivity extends AppCompatActivity {
 
     private View[] stickers;
 
-    private void faceStickersRotate(int face) {
-        char tempEdge = cube[face][1];
+    private void faceStickersRotate(int face) {//rotate the stickers on a certain given face clockwise. This isn't a complete turn because
+        char tempEdge = cube[face][1];         //other stickers on other faces must be done to be a complete turn.
         char tempCorner = cube[face][0];
 
         cube[face][0] = cube[face][6]; //for "corner stickers"
@@ -44,8 +45,6 @@ public class MainActivity extends AppCompatActivity {
         cube[face][7] = cube[face][5];
         cube[face][5] = cube[face][3];
         cube[face][3] = tempEdge;
-
-
     }
 
     private void printCube() {
@@ -75,6 +74,142 @@ public class MainActivity extends AppCompatActivity {
                 count++;
             }
         }
+    }
+
+    private void clockwiseU() {//simulates turning the whole designated face of the cube clockwise 90 degrees
+        faceStickersRotate(2);
+        char tmp1 = cube[4][0];
+        char tmp2 = cube[4][1];
+        char tmp3 = cube[4][2];
+
+        cube[4][0] = cube[3][6];
+        cube[4][1] = cube[3][7];
+        cube[4][2] = cube[3][0];
+
+        cube[3][6] = cube[0][4];
+        cube[3][7] = cube[0][5];
+        cube[3][0] = cube[0][6];
+
+        cube[0][4] = cube[1][2];
+        cube[0][5] = cube[1][3];
+        cube[0][6] = cube[1][4];
+
+        cube[1][2] = tmp1;
+        cube[1][3] = tmp2;
+        cube[1][4] = tmp3;
+    }
+
+    private void clockwiseR() {
+        faceStickersRotate(3);
+        char tmp2 = cube[2][2];
+        char tmp3 = cube[2][3];
+        char tmp4 = cube[2][4];
+
+        cube[2][2] = cube[4][2];
+        cube[2][3] = cube[4][3];
+        cube[2][4] = cube[4][4];
+
+        cube[4][2] = cube[5][2];
+        cube[4][3] = cube[5][3];
+        cube[4][4] = cube[5][4];
+
+        cube[5][2] = cube[0][2];
+        cube[5][3] = cube[0][3];
+        cube[5][4] = cube[0][4];
+
+        cube[0][2] = tmp2;
+        cube[0][3] = tmp3;
+        cube[0][4] = tmp4;
+    }
+
+    private void clockwiseF() {
+        faceStickersRotate(4);
+        char tmp6 = cube[2][6];
+        char tmp5 = cube[2][5];
+        char tmp4 = cube[2][4];
+
+        cube[2][6] = cube[1][6];
+        cube[2][5] = cube[1][5];
+        cube[2][4] = cube[1][4];
+
+        cube[1][6] = cube[5][2];
+        cube[1][5] = cube[5][1];
+        cube[1][4] = cube[5][0];
+
+        cube[5][2] = cube[3][6];
+        cube[5][1] = cube[3][5];
+        cube[5][0] = cube[3][4];
+
+        cube[3][6] = tmp6;
+        cube[3][5] = tmp5;
+        cube[3][4] = tmp4;
+    }
+
+    private void clockwiseD() {
+        faceStickersRotate(5);
+        char tmp6 = cube[4][6];
+        char tmp5 = cube[4][5];
+        char tmp4 = cube[4][4];
+
+        cube[4][6] = cube[1][0];
+        cube[4][5] = cube[1][7];
+        cube[4][4] = cube[1][6];
+
+        cube[1][0] = cube[0][2];
+        cube[1][7] = cube[0][1];
+        cube[1][6] = cube[0][0];
+
+        cube[0][2] = cube[3][4];
+        cube[0][1] = cube[3][3];
+        cube[0][0] = cube[3][2];
+
+        cube[3][4] = tmp6;
+        cube[3][3] = tmp5;
+        cube[3][2] = tmp4;
+    }
+    private void clockwiseL() {
+        faceStickersRotate(1);
+        char tmp0 = cube[2][0];
+        char tmp7 = cube[2][7];
+        char tmp6 = cube[2][6];
+
+        cube[2][0] = cube[0][0];
+        cube[2][7] = cube[0][7];
+        cube[2][6] = cube[0][6];
+
+        cube[0][0] = cube[5][0];
+        cube[0][7] = cube[5][7];
+        cube[0][6] = cube[5][6];
+
+        cube[5][0] = cube[4][0];
+        cube[5][7] = cube[4][7];
+        cube[5][6] = cube[4][6];
+
+        cube[4][0] = tmp0;
+        cube[4][7] = tmp7;
+        cube[4][6] = tmp6;
+    }
+    private void clockwiseB() {
+        faceStickersRotate(0);
+        char tmp0 = cube[2][0];
+        char tmp1 = cube[2][1];
+        char tmp2 = cube[2][2];
+
+        cube[2][0] = cube[3][0];
+        cube[2][1] = cube[3][1];
+        cube[2][2] = cube[3][2];
+
+        cube[3][0] = cube[5][4];
+        cube[3][1] = cube[5][5];
+        cube[3][2] = cube[5][6];
+
+        cube[5][4] = cube[1][0];
+        cube[5][5] = cube[1][1];
+        cube[5][6] = cube[1][2];
+
+        cube[1][0] = tmp0;
+        cube[1][1] = tmp1;
+        cube[1][2] = tmp2;
     }
 
     @Override
@@ -160,56 +295,97 @@ public class MainActivity extends AppCompatActivity {
         turnU.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            faceStickersRotate(2);
-            char tmp1 = cube[4][0];
-            char tmp2 = cube[4][1];
-            char tmp3 = cube[4][2];
-
-            cube[4][0] = cube[3][6];
-            cube[4][1] = cube[3][7];
-            cube[4][2] = cube[3][0];
-
-            cube[3][6] = cube[0][4];
-            cube[3][7] = cube[0][5];
-            cube[3][0] = cube[0][6];
-
-            cube[0][4] = cube[1][2];
-            cube[0][5] = cube[1][3];
-            cube[0][6] = cube[1][4];
-
-            cube[1][2] = tmp1;
-            cube[1][3] = tmp2;
-            cube[1][4] = tmp3;
+            clockwiseU();
             printCube();
             }
         });
         turnUi.setOnClickListener(new View.OnClickListener() {
             @Override
+            public void onClick(View v) {// for any turnXi (i denotes counter-clockwise) it is the same as turning that face clockwise 3 times
+            clockwiseU();
+            clockwiseU();
+            clockwiseU();
+            printCube();
+            }
+        });
+        turnR.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                faceStickersRotate(2);
-                char tmp1 = cube[4][0];
-                char tmp2 = cube[4][1];
-                char tmp3 = cube[4][2];
-
-                cube[4][0] = cube[1][2];
-                cube[4][1] = cube[1][3];
-                cube[4][2] = cube[1][4];
-
-                cube[1][2] = cube[0][4];
-                cube[1][3] = cube[0][5];
-                cube[1][4] = cube[0][6];
-
-                cube[0][4] = cube[3][6];
-                cube[0][5] = cube[3][7];
-                cube[0][6] = cube[3][0];
-
-                cube[3][6] = tmp1;
-                cube[3][7] = tmp2;
-                cube[3][0] = tmp3;
-
-
-
-                printCube();
+            clockwiseR();
+            printCube();
+            }
+        });
+        turnRi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            clockwiseR();
+            clockwiseR();
+            clockwiseR();
+            printCube();
+            }
+        });
+        turnF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            clockwiseF();
+            printCube();
+            }
+        });
+        turnFi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            clockwiseF();
+            clockwiseF();
+            clockwiseF();
+            printCube();
+            }
+        });
+        turnD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            clockwiseD();
+            printCube();
+            }
+        });
+        turnDi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            clockwiseD();
+            clockwiseD();
+            clockwiseD();
+            printCube();
+            }
+        });
+        turnL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            clockwiseL();
+            printCube();
+            }
+        });
+        turnLi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            clockwiseL();
+            clockwiseL();
+            clockwiseL();
+            printCube();
+            }
+        });
+        turnB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            clockwiseB();
+            printCube();
+            }
+        });
+        turnBi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            clockwiseB();
+            clockwiseB();
+            clockwiseB();
+            printCube();
             }
         });
     }
